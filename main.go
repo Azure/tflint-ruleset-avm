c7009cdd976e6806ff92a94e11ed7b871dc42534
hezijie <lonegunmanb@hotmail.com>
2024-01-25 15:42:32 +0800

    correct reference path

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"github.com/Azure/tflint-ruleset-avm/rules"
 	"github.com/terraform-linters/tflint-plugin-sdk/plugin"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
-	"github.com/terraform-linters/tflint-ruleset-template/rules"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "template",
-			Version: "0.1.0",
+			Version: "v0.1.0",
 			Rules:   rules.Rules,
 		},
 	})
