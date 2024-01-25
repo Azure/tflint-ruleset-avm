afe8ca2f93e6a2e29e8123719633012320cc9e06
hezijie <lonegunmanb@hotmail.com>
2024-01-25 16:38:20 +0800

    try ado release pipeline

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary.
+	version string = "dev"
+
+	// goreleaser can pass other information to the main package, such as the specific commit
+	// https://goreleaser.com/cookbooks/using-main.version/
+)
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "template",
-			Version: "v0.1.0",
+			Version: version,
 			Rules:   rules.Rules,
 		},
 	})
