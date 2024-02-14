29b86eee8e8d2112a55cff38bba23cc273d98925
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-02-14 13:57:16 +0000

    feat: lock interface complete

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -7,27 +7,22 @@ import (
 )
 
 // Interfaces represent the standard AVM interfaces that are checked by rules.AVMInterfaceRule.
-type Interface struct {
-	Name            string           // Name of the interface.
-	Link            string           // Link to the interface documentation.
-	Type            string           // String representing the type value.
-	Enabled         bool             // Whether to test this interface interface.
-	Default         cty.Value        // Default value for the interface in cty.
-	ValidationRules []ValidationRule // Validation rules for the interface.
+type AVMInterface struct {
+	Name    string    // Name of the interface.
+	Link    string    // Link to the interface documentation.
+	Type    string    // String representing the type value.
+	Enabled bool      // Whether to test this interface interface.
+	Default cty.Value // Default value for the interface in cty.
+	// TODO: add validation rule checks
 }
 
-type ValidationRule struct {
-	Condition    string
-	ErrorMessage string
-}
-
-var AllInterfaces = []Interface{
+var AllInterfaces = []AVMInterface{
 	Lock,
 }
 
 // TypeExpression returns an HCL expression that represents the interface type.
 // If the interface cannot be correctly parsed, this function will panic.
-func (i Interface) TypeExpression() hcl.Expression {
+func (i AVMInterface) TypeExpression() hcl.Expression {
 	e, d := hclsyntax.ParseExpression([]byte(i.Type), "variables.tf", hcl.Pos{})
 	if d.HasErrors() {
 		panic(d.Error())
