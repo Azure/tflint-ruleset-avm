39aaee296e2367825064d76e0dadca5ba36ddf5b
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-03-02 16:32:05 +0000

    fix: incorrect resource type

--- waf/azurermStorageAccount.go
+++ waf/azurermStorageAccount.go
@@ -4,7 +4,7 @@ import "github.com/Azure/tflint-ruleset-avm/attrvalue"
 
 var AzurermStorageAccountAccountReplicationType = func() *attrvalue.AttrStringValueRule {
 	return attrvalue.NewAttrStringValueRule(
-		"azurerm_key_vault",
+		"azurerm_storage_account",
 		"account_replication_type",
 		[]string{"ZRS"},
 	)
