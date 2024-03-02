32940b392d1f21f72b633d85b5ef4298c6851fc7
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-03-02 16:28:58 +0000

    chore: rename func

--- waf/azurermStorageAccount.go
+++ waf/azurermStorageAccount.go
@@ -2,7 +2,7 @@ package waf
 
 import "github.com/Azure/tflint-ruleset-avm/attrvalue"
 
-var AzurermKeyVaultAccountReplicationType = func() *attrvalue.AttrStringValueRule {
+var AzurermStorageAccountAccountReplicationType = func() *attrvalue.AttrStringValueRule {
 	return attrvalue.NewAttrStringValueRule(
 		"azurerm_key_vault",
 		"account_replication_type",
--- waf/waf.go
+++ waf/waf.go
@@ -3,5 +3,5 @@ package waf
 import "github.com/terraform-linters/tflint-plugin-sdk/tflint"
 
 var Rules = []tflint.Rule{
-	AzurermKeyVaultAccountReplicationType(),
+	AzurermStorageAccountAccountReplicationType(),
 }
