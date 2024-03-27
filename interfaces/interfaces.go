c3a43c2d411ee4d57e5f281eac620a8f53b68ea2
hezijie <lonegunmanb@hotmail.com>
2024-03-27 14:18:14 +0800

    keep refactor

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -5,17 +5,15 @@ import (
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
-var privateEndpointsWithoutSubresourceNameRule = NewVarCheckRuleFromAvmInterface(PrivateEndpoints)
-var privateEndpointsWithSubresourceNameRule = NewVarCheckRuleFromAvmInterface(PrivateEndpointsWithSubresourceName)
-var PrivateEndpointsRule = common.NewEitherCheckRule("private_endpoints", true, tflint.ERROR,
-	privateEndpointsWithoutSubresourceNameRule,
-	privateEndpointsWithSubresourceNameRule)
-
 var Rules = []tflint.Rule{
 	NewVarCheckRuleFromAvmInterface(Lock),
 	NewVarCheckRuleFromAvmInterface(DiagnosticSettings),
 	NewVarCheckRuleFromAvmInterface(ManagedIdentities),
 	NewVarCheckRuleFromAvmInterface(RoleAssignments),
 	NewVarCheckRuleFromAvmInterface(CustomerManagedKey),
-	PrivateEndpointsRule,
+	func() tflint.Rule {
+		return common.NewEitherCheckRule("private_endpoints", true, tflint.ERROR,
+			NewVarCheckRuleFromAvmInterface(PrivateEndpoints),
+			NewVarCheckRuleFromAvmInterface(PrivateEndpointsWithSubresourceName))
+	}(),
 }
