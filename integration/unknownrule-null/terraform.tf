a16d4b99e6eb5c1a656edc6b70a2edd31e5ce64c
neil-yechenwei <yechenwei2007@hotmail.com>
2024-04-18 17:37:11 +0800

    update code

--- integration/unknownrule-null/terraform.tf
+++ integration/unknownrule-null/terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "~> 1.7.0"
   required_providers {
     azurerm = {
-      version = ">= 3.97.0, < 3.99.0"
       source  = "hashicorp/azurerm"
+      version = ">= 3.97.0, < 3.99.0"
     }
   }
 }
