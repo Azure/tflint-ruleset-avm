2caf1dd46809cc75229da14f56b6771b2fa2dbe5
neil-yechenwei <yechenwei2007@hotmail.com>
2024-04-19 14:42:28 +0800

    update code

--- rules/modules_test.go
+++ rules/modules_test.go
@@ -16,18 +16,9 @@ func TestModules(t *testing.T) {
 		{
 			desc: "source exists, ok",
 			files: map[string]string{
-				"terraform.tf": `terraform {
-  required_version = "~> 1.7.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.97.0, < 3.99.0"
-    }
-  }
-  module "other-module" {
-    source  = "Azure/terraform-azurerm-avm-res-keyvault-vault"
-    version = "0.5.3"
-  }
+				"terraform.tf": `module "other-module" {
+  source  = "Azure/terraform-azurerm-avm-res-keyvault-vault"
+  version = "0.5.3"
 }`,
 			},
 			issues: helper.Issues{},
