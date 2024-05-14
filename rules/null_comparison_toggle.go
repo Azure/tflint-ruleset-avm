c2d0cfb487125bae4481beb15946c6665c6ae363
neil-yechenwei <yechenwei2007@hotmail.com>
2024-05-14 17:48:34 +0800

    update code

--- rules/null_comparison_toggle.go
+++ rules/null_comparison_toggle.go
@@ -1,6 +1,7 @@
 package rules
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/hashicorp/go-multierror"
@@ -12,6 +13,15 @@ import (
 
 var nullComparisonToggleBodySchema = &hclext.BodySchema{
 	Blocks: []hclext.BlockSchema{
+		{
+			Type:       "variable",
+			LabelNames: []string{"name"},
+			Body: &hclext.BodySchema{
+				Attributes: []hclext.AttributeSchema{
+					{Name: "type"},
+				},
+			},
+		},
 		{
 			Type:       "resource",
 			LabelNames: []string{"type", "name"},
@@ -68,13 +78,26 @@ func (t *NullComparisonToggleRule) Check(r tflint.Runner) error {
 		return err
 	}
 
+	var resourceIdStr string
 	var errList error
 	for _, block := range body.Blocks {
 		if block.Type != "resource" {
 			continue
 		}
 
-		if subErr := t.checkBlock(r, block); subErr != nil {
+		resourceIdString, subErr := t.checkResourceBlock(r, block)
+		if subErr != nil {
+			errList = multierror.Append(errList, subErr)
+		}
+		resourceIdStr = resourceIdString
+	}
+
+	for _, block := range body.Blocks {
+		if block.Type != "variable" {
+			continue
+		}
+
+		if subErr := t.checkVariableBlock(r, block, resourceIdStr); subErr != nil {
 			errList = multierror.Append(errList, subErr)
 		}
 	}
@@ -82,38 +105,68 @@ func (t *NullComparisonToggleRule) Check(r tflint.Runner) error {
 	return errList
 }
 
-func (t *NullComparisonToggleRule) checkBlock(r tflint.Runner, block *hclext.Block) error {
+func (t *NullComparisonToggleRule) checkResourceBlock(r tflint.Runner, block *hclext.Block) (string, error) {
 	count, exists := block.Body.Attributes["count"]
 	if !exists {
-		return nil
+		return "", nil
 	}
 
 	countConditionalExpr, ok := count.Expr.(*hclsyntax.ConditionalExpr)
 	if !ok {
-		return nil
+		return "", nil
 	}
 
+	var resourceIdStr string
+	var err error
 	for _, dynamicObj := range countConditionalExpr.Variables() {
-		if err := t.checkDynamicObject(r, dynamicObj, count.Range); err != nil {
-			return err
+		resourceIdStr, err = t.checkCountBlock(r, dynamicObj, count.Range)
+		if err != nil {
+			return resourceIdStr, err
 		}
 	}
 
-	return nil
+	return resourceIdStr, nil
 }
 
-func (t *NullComparisonToggleRule) checkDynamicObject(r tflint.Runner, dynamicObj hcl.Traversal, rangeInfo hcl.Range) error {
+func (t *NullComparisonToggleRule) checkCountBlock(r tflint.Runner, dynamicObj hcl.Traversal, rangeInfo hcl.Range) (string, error) {
 	for _, dynamicVal := range dynamicObj {
 		if v, ok := dynamicVal.(hcl.TraverseRoot); ok && strings.HasSuffix(v.Name, "local") {
 			break
 		}
 
 		if v, ok := dynamicVal.(hcl.TraverseAttr); ok && strings.HasSuffix(strings.ToLower(v.Name), "_id") {
-			return r.EmitIssue(
+			return v.Name, r.EmitIssue(
 				t,
 				"The variable should be defined as object type for the resource id",
 				rangeInfo,
 			)
+		}
+	}
+
+	return "", nil
+}
+
+func (t *NullComparisonToggleRule) checkVariableBlock(r tflint.Runner, block *hclext.Block, resourceIdStr string) error {
+	for _, label := range block.Labels {
+		if label == resourceIdStr {
+			attr, exists := block.Body.Attributes["type"]
+			if !exists {
+				return r.EmitIssue(
+					t,
+					fmt.Sprintf("`%s` type not declared", label),
+					attr.Range,
+				)
+			}
+
+			for _, dynamicVal := range attr.Expr.Variables() {
+				if v := dynamicVal.RootName(); v == "string" {
+					return r.EmitIssue(
+						t,
+						"The variable should be defined as object type for the resource id",
+						dynamicVal.SourceRange(),
+					)
+				}
+			}
 		}
 	}
 
