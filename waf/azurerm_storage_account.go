c95b10d65db9b53f5f23ea4fd3577de42b08e67c
chinthakaru <chinthakaru@gmail.com>
2024-11-07 07:19:58 +0530

    Updated the Accout_Replication_Level to GRS

--- waf/azurerm_storage_account.go
+++ waf/azurerm_storage_account.go
@@ -8,7 +8,7 @@ func (wf WafRules) AzurermStorageAccountAccountReplicationType() *attrvalue.Simp
 	return attrvalue.NewSimpleRule[string](
 		"azurerm_storage_account",
 		"account_replication_type",
-		[]string{"ZRS"},
+		[]string{"GRS"},
 		"https://azure.github.io/Azure-Proactive-Resiliency-Library/services/storage/storage-account/#st-1---ensure-that-storage-accounts-are-zone-or-region-redundant",
 		false,
 		"",
