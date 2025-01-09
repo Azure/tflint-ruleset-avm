44244b8d33f1339099a68ad3abf5f997298bafac
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-01-09 15:18:45 +0000

    Update waf/azurerm_compute_virtualmachinescaleset.go

--- waf/azurerm_compute_virtualmachinescaleset.go
+++ waf/azurerm_compute_virtualmachinescaleset.go
@@ -18,7 +18,7 @@ func (wf WafRules) AutomaticRepairsPolicyEnabledLinux() *attrvalue.SimpleRule[bo
 func (wf WafRules) AutomaticRepairsPolicyEnabledWindows() *attrvalue.SimpleRule[bool] {
 	return attrvalue.NewSimpleRule[bool](
 		"azurerm_windows_virtual_machine_scale_set",
-		"automatic_instance_repair ",
+		"automatic_instance_repair",
 		[]bool{true},
 		"https://azure.github.io/Azure-Proactive-Resiliency-Library-v2/azure-resources/Compute/virtualMachineScaleSets/#enable-automatic-repair-policy-on-azure-virtual-machine-scale-sets",
 		false,
