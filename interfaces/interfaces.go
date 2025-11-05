19113484bb0f9af9a11600ee8b32a348ce2d26ea
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-11-05 17:25:44 +0000

    feat: simplify private endpoints

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,7 +1,6 @@
 package interfaces
 
 import (
-	"github.com/Azure/tflint-ruleset-avm/common"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
@@ -13,9 +12,5 @@ var Rules = []tflint.Rule{
 	NewVarCheckRuleFromAvmInterface(ManagedIdentities),
 	NewVarCheckRuleFromAvmInterface(RoleAssignments),
 	NewVarCheckRuleFromAvmInterface(Tags),
-	func() tflint.Rule {
-		return common.NewEitherCheckRule("private_endpoints", true, tflint.ERROR,
-			NewVarCheckRuleFromAvmInterface(PrivateEndpoints),
-			NewVarCheckRuleFromAvmInterface(PrivateEndpointsWithSubresourceName))
-	}(),
+	NewVarCheckRuleFromAvmInterface(PrivateEndpoints),
 }
--- interfaces/private_endpoints.go
+++ interfaces/private_endpoints.go
@@ -23,6 +23,7 @@ var PrivateEndpointTypeString = `map(object({
     name = optional(string, null)
   }), null)
   tags               = optional(map(string), null)
+	subresource_name   = optional(string, null)
   subnet_resource_id = string
   private_dns_zone_group_name             = optional(string, "default")
   private_dns_zone_resource_ids           = optional(set(string), [])
